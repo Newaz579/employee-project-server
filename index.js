258eeec1a12d72148007f10f35bf68c47a5636ec
newaz579 <newazsharif579@gmail.com>
2024-09-11 15:58:39 +0600

    crud operation done

--- index.js
+++ index.js
@@ -1,3 +1,132 @@
+const http = require('http');
+const url = require('url');
+const path = require('path');
+const fs = require('fs');
+const {
+    addEmployeeController,
+    searchEmployeeController,
+    getSingleEmployeeController,
+    updateEmployeeController,
+    deleteEmployeeController,
+    getAllEmployeeController
+} = require('./controller/controller');
+
+// Function to serve static files
+function serveStaticFile(filePath, contentType, res) {
+    fs.readFile(filePath, (err, content) => {
+        if (err) {
+            // console.error(`Error reading file ${filePath}:`, err);
+            res.writeHead(500);
+            res.end('Error occurred');
+        } else {
+            res.writeHead(200, { 'Content-Type': contentType });
+            res.end(content, 'utf-8');
+        }
+    });
+}
+
+// Create HTTP server
+const server = http.createServer((req, res) => {
+    const parsedUrl = url.parse(req.url, true);
+    const pathname = parsedUrl.pathname;
+    let body = '';
+
+    // Handle CORS
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+
+    req.on('data', chunk => {
+        body += chunk.toString();
+    });
+
+    req.on('end', () => {
+        // console.log(`Request URL: ${req.url}, Method: ${req.method}`); // Debug
+
+        if (pathname === '/') {
+            serveStaticFile(path.join(__dirname, 'view', 'index.html'), 'text/html', res);
+        } else if (pathname === '/addEmployee') {
+            serveStaticFile(path.join(__dirname, 'view', 'addEmployee.html'), 'text/html', res);
+        } else if (pathname === '/updateEmployee') {
+            serveStaticFile(path.join(__dirname, 'view', 'updateEmployee.html'), 'text/html', res);
+        } else if (pathname === '/add-employee') {
+            addEmployeeController(body, res);
+        } else if (pathname === '/search-employees') {
+            searchEmployeeController(parsedUrl, res);
+        } else if (pathname.startsWith('/getsingle-employee/')) {
+            getSingleEmployeeController(pathname, res);
+        } else if (req.method === 'PUT' && pathname.startsWith('/update-employee/')) {
+            updateEmployeeController(pathname, body, res);
+        } else if (req.method === 'DELETE' && pathname.startsWith('/delete-employee/')) {
+            deleteEmployeeController(pathname, res);
+        } else if (req.method === 'GET' && pathname === '/getAll') {
+            getAllEmployeeController(res);
+        } else {
+            // console.log(`Route not found: ${pathname}`); // Debug
+            res.writeHead(404, { 'Content-Type': 'text/plain' });
+            res.end('Not Found');
+        }
+    });
+});
+
+const PORT = 5000;
+server.listen(PORT, () => {
+    console.log(`Server running on port http://localhost:${PORT}`);
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 // const http = require('http');
 // const url = require('url');
 // const path = require('path');
@@ -100,84 +229,3 @@
 
 
 
-
-
-
-
-
-
-const http = require('http');
-const url = require('url');
-const path = require('path');
-const fs = require('fs');
-const {
-    addEmployeeController,
-    searchEmployeeController,
-    getSingleEmployeeController,
-    updateEmployeeController,
-    deleteEmployeeController,
-    getAllEmployeeController
-} = require('./controller/controller');
-
-// Function to serve static files
-function serveStaticFile(filePath, contentType, res) {
-    fs.readFile(filePath, (err, content) => {
-        if (err) {
-            // console.error(`Error reading file ${filePath}:`, err);
-            res.writeHead(500);
-            res.end('Error occurred');
-        } else {
-            res.writeHead(200, { 'Content-Type': contentType });
-            res.end(content, 'utf-8');
-        }
-    });
-}
-
-// Create HTTP server
-const server = http.createServer((req, res) => {
-    const parsedUrl = url.parse(req.url, true);
-    const pathname = parsedUrl.pathname;
-    let body = '';
-
-    // Handle CORS
-    res.setHeader('Access-Control-Allow-Origin', '*');
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
-    res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
-
-    req.on('data', chunk => {
-        body += chunk.toString();
-    });
-
-    req.on('end', () => {
-        // console.log(`Request URL: ${req.url}, Method: ${req.method}`); // Debug
-
-        if (pathname === '/') {
-            serveStaticFile(path.join(__dirname, 'view', 'index.html'), 'text/html', res);
-        } else if (pathname === '/addEmployee') {
-            serveStaticFile(path.join(__dirname, 'view', 'addEmployee.html'), 'text/html', res);
-        } else if (pathname === '/updateEmployee') {
-            serveStaticFile(path.join(__dirname, 'view', 'updateEmployee.html'), 'text/html', res);
-        } else if (pathname === '/add-employee') {
-            addEmployeeController(body, res);
-        } else if (pathname === '/search-employees') {
-            searchEmployeeController(parsedUrl, res);
-        } else if (pathname.startsWith('/getsingle-employee/')) {
-            getSingleEmployeeController(pathname, res);
-        } else if (req.method === 'PUT' && pathname.startsWith('/update-employee/')) {
-            updateEmployeeController(pathname, body, res);
-        } else if (req.method === 'DELETE' && pathname.startsWith('/delete-employee/')) {
-            deleteEmployeeController(pathname, res);
-        } else if (req.method === 'GET' && pathname === '/getAll') {
-            getAllEmployeeController(res);
-        } else {
-            // console.log(`Route not found: ${pathname}`); // Debug
-            res.writeHead(404, { 'Content-Type': 'text/plain' });
-            res.end('Not Found');
-        }
-    });
-});
-
-const PORT = 5000;
-server.listen(PORT, () => {
-    console.log(`Server running on port http://localhost:${PORT}`);
-});
